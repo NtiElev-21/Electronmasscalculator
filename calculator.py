f641231d9bf29f4a9dcebd6356cdd23fb77b094e
NtiElev-21 <156201377+NtiElev-21@users.noreply.github.com>
2024-05-29 22:24:20 +0200

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,3 +1,12 @@
-A = float(input("Ström (A)"))
-U = float(input("Spänning (V)"))
+a = float(input("Ström (A)"))
+u = float(input("Spänning (V)"))
 r = float(input("radie (meter)"))
+
+m = ((((r**2)*(1.602)*(10**(-19)))/(2*u))*((((541)/(781250))*a)**2))
+
+
+mprefix = m*(10**(-31))
+
+mprefixshort = round(mprefix, 5)
+
+print("Massan av elektronen är estimerad till ",mprefixshort,"!")
